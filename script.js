29e0bd53c77e73d7a03c57a0efb75aefc69cc884
mkkieru <mark.kieru@student.moringaschool.com>
2021-04-30 01:24:14 +0300

    add final editings

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
-    $("#design").click(function(){
+$(document).ready(function() {
+    $("#design").click(function() {
         $(".show").slideToggle();
-       
+
     });
-    $("#design2").click(function(){
+    $("#design2").click(function() {
         $(".showing").slideToggle();
-       
+
     });
 });
